50dd0adcf76111e6b650273ec6637c076d13bb01
하민철 <minchulha0703@gmail.com>
2024-02-08 23:58:25 -0500

    Add styled components with concept of adapting, extending, and as

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import styled from 'styled-components';
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
+`;
+
 function App() {
-  return null;
+  return(
+    <Father>
+      <Btn>Log in</Btn>
+      <Btn as="a" href="/">
+        Log in
+      </Btn>
+    </Father>
+  );
 }
 
 export default App;
