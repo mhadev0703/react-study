7831e42b9b4dea97ecd94a78546bb30adc45e7a7
하민철 <minchulha0703@gmail.com>
2024-02-09 23:57:59 -0500

    modify pseudo selector part

--- src/App.js
+++ src/App.js
@@ -2,38 +2,27 @@ import styled, { keyframes } from 'styled-components';
 
 const Wrapper = styled.div`
   display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
 `;
 
 const rotationAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50% {
-    border-radius: 100px;
-  }
-  100% {
-    transform: rotate(360deg);
-    border-radius: 0px;
-  }
+
+`;
+
+const Emoji = styled.span`
+  font-size: 36px;
 `;
 
 const Box = styled.div`
   height: 200px;
   width: 200px;
-  background-color: tomato;
-  display: flex;
   justify-content: center;
   align-items: center;
   animation: ${rotationAnimation} 1s linear infinite;
-  span {
-    font-size: 36px;
-    &: hover {
-      font-size: 50px;
-    }
-    &: active {
-      opacity: 0;
-    }
+  ${Emoji}:hover {
+    font-size: 98px;
   }
 `;
 
@@ -41,8 +30,9 @@ function App() {
   return(
     <Wrapper>
       <Box>
-        <span>🤩</span>
+        <Emoji>🤩</Emoji>
       </Box>
+      <Emoji>🔥</Emoji>
     </Wrapper>
   );
 }
