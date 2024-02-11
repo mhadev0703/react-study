dff1309ff760bc0ee39144423a3444ab00e972e4
하민철 <minchulha0703@gmail.com>
2024-02-10 23:57:48 -0500

    fix code of Box component for targeting an element

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import styled, { keyframes } from 'styled-components';
 
 const Wrapper = styled.div`
   display: flex;
-  height: 100vh;
-  justify-content: center;
-  align-items: center;
 `;
 
 const rotationAnimation = keyframes`
-
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    border-radius: 100px;
+  }
+  100% {
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
 `;
 
 const Emoji = styled.span`
@@ -18,11 +25,15 @@ const Emoji = styled.span`
 const Box = styled.div`
   height: 200px;
   width: 200px;
+  background-color: tomato;
+  display: flex;
   justify-content: center;
   align-items: center;
   animation: ${rotationAnimation} 1s linear infinite;
-  ${Emoji}:hover {
-    font-size: 98px;
+  ${Emoji} {
+    &: hover {
+      font-size: 98px;
+    }
   }
 `;
 
