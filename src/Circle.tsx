4c0cc5a33715ca0211f5e32d800fa58ae2720c0f
하민철 <minchulha0703@gmail.com>
2024-02-12 23:58:35 -0500

    apply concept of typing props

---
+++ src/Circle.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+
+interface ContainerProps {
+    bgColor: string;
+}
+
+const Container = styled.div<ContainerProps>`
+    width: 200px;
+    height: 200px;
+    background-color: ${(props) => props.bgColor};
+`;
+
+interface CircleProps {
+    bgColor: string;
+}
+
+function Circle({ bgColor }: CircleProps) {
+    return <Container bgColor={bgColor} />;
+}
+
+export default Circle;
