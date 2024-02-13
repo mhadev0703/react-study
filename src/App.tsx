ea88a61d5732e019a9d0a60d1cec98a83068df62
하민철 <minchulha0703@gmail.com>
2024-02-13 16:36:15 -0500

    Implement extend styled-compoents types with themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,33 @@
-import { on } from 'events';
 import React, { useState } from 'react';
+import { styled } from 'styled-components';
+
+const Container = styled.div`
+  background-color: ${props => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+interface DummyProps {
+  text: string;
+  active?: boolean;
+}
+
+function Dummy({ text, active = false }: DummyProps) {
+  return <H1>{text}</H1>
+}
 
 function App() {
-
-  const [value, setValue] = useState("");
-
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const { currentTarget: { value } } = event;
-    setValue(value);
-  };
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => { 
-    event.preventDefault();
-    console.log("Hello", value);
+  const onClick = (event:React.MouseEvent<HTMLButtonElement>) => {
+    
   };
 
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input 
-          value={value} 
-          onChange={onChange} 
-          type="text" 
-          placeholder="username" 
-        />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <Dummy active text="Hello" />
+      <button onClick={onClick}>Click me</button> 
+    </Container>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider theme={darkTheme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>
 );
 
 
