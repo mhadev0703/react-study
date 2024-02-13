5a5afd51456f2b7139d11bae9321af466b87a35c
하민철 <minchulha0703@gmail.com>
2024-02-13 15:07:56 -0500

    add form events with explicit typing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import styled from 'styled-components';
-import React from 'react';
-import Circle from "./Circle";
+import { on } from 'events';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [value, setValue] = useState("");
+
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const { currentTarget: { value } } = event;
+    setValue(value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => { 
+    event.preventDefault();
+    console.log("Hello", value);
+  };
+
   return (
     <div>
-      <Circle borderColor="yellow" bgColor="teal" />
-      <Circle text="I'm here" bgColor="tomato" />
+      <form onSubmit={onSubmit}>
+        <input 
+          value={value} 
+          onChange={onChange} 
+          type="text" 
+          placeholder="username" 
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
