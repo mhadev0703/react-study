8b6950bbf99fcf9c29346e80b6a616afe34d9ee2
하민철 <minchulha0703@gmail.com>
2024-02-13 11:50:34 -0500

    apply optional props

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -3,20 +3,29 @@ import styled from "styled-components";
 
 interface ContainerProps {
     bgColor: string;
+    borderColor: string;
 }
 
 const Container = styled.div<ContainerProps>`
     width: 200px;
     height: 200px;
     background-color: ${(props) => props.bgColor};
+    border-radius: 100px;
+    border: 1px solid ${(props) => props.borderColor}
 `;
 
 interface CircleProps {
     bgColor: string;
+    borderColor?: string;
+    text?: string;
 }
 
-function Circle({ bgColor }: CircleProps) {
-    return <Container bgColor={bgColor} />;
+function Circle({ bgColor, borderColor, text = "Default text" }: CircleProps) {
+  return (
+    <Container bgColor={ bgColor} borderColor={ borderColor ?? bgColor }>
+      {text}
+    </Container>
+  );    
 }
 
 export default Circle;
--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from 'styled-components';
 import App from './App';
-
-const darkTheme = {
-  textColor: "whitesmoke",
-  backgroundColor: "#111"
-}
-
-const lightTheme = {
-  textColor: "#111",
-  backgroundColor: "whitesmoke"
-}
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={lightTheme}>
-      <App />
-    </ThemeProvider>    
+    <App />
   </React.StrictMode>
 );
 
