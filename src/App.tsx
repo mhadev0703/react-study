aa7b9aa7dce2213c2f689f040cf2d3c0e2c1fc4c
하민철 <minchulha0703@gmail.com>
2024-02-15 23:57:30 -0500

    inital setup of react router using BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
-import { styled } from 'styled-components';
+import Router from './Router';
 
-const Container = styled.div`
-  background-color: ${props => props.theme.bgColor};
-`;
-
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
 function App() {
-  const onClick = (event:React.MouseEvent<HTMLButtonElement>) => {};
 
   return (
-    <Container>
-      <button onClick={onClick}>Click me</button> 
-    </Container>
+    <div>
+      <Router />
+    </div>
   );
 }
 
