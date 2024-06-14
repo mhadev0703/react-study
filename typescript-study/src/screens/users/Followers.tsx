8cbf0fd6261ac91bb73ee4ca009b2d8389ecb174
mhadev0703 <minchulha0703@gmail.com>
2024-06-13 23:55:50 -0400

    Reorganize project structure

---
+++ typescript-study/src/screens/users/Followers.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useOutletContext } from "react-router-dom";
+
+interface IFollowersContext {
+    nameOfMyUser: string;
+}
+
+function Followers() {
+    const { nameOfMyUser } = useOutletContext<IFollowersContext>();
+    return <h1>Here are {nameOfMyUser}'s followers</h1>;
+}
+
+export default Followers;
