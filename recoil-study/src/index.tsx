a42fdaeba9a205479b985796625b7b81aed0a805
mhadev0703 <minchulha0703@gmail.com>
2024-06-15 23:59:30 -0400

    index.tsx file initial setup

--- recoil-study/src/index.tsx
+++ recoil-study/src/index.tsx
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from 'styled-components';
+import { darkTheme } from './theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <ThemeProvider theme={darkTheme}>
+        <App />
+      </ThemeProvider>
+    </RecoilRoot>
   </React.StrictMode>
 );
 
