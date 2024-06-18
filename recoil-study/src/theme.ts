8522f21ce824dccc820f976a1b060447123f0bfc
mhadev0703 <minchulha0703@gmail.com>
2024-06-17 23:59:10 -0400

    Add project files

---
+++ recoil-study/src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+    bgColor: "#2f3640",
+    nameColor: "black", 
+    textColor: "whitesmoke",
+    accentColor: "#9c88ff",
+};
