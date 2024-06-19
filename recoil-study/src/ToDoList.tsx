5dda26243cddd8cd2a0ad55674b2693b2ccef4ec
mhadev0703 <minchulha0703@gmail.com>
2024-06-19 18:39:57 -0400

    Add input form with react hook form

--- recoil-study/src/ToDoList.tsx
+++ recoil-study/src/ToDoList.tsx
@@ -1,5 +1,52 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+/* function ToDoList() {
+  const [toDo, setToDo] = useState("");
+  const [toDoError, setToDoError] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setToDo(value);
+  };
+
+  const onSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (toDo.length < 10) {
+      return setToDoError("To do should be longer");
+    }
+    console.log("submit");
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} placeholder="Write a to do" />
+        <button>Add</button>
+        {toDoError !== "" ? toDoError : null}
+      </form>
+    </>
+  );
+} */
+
 function ToDoList() {
-    return null;
+  const { register, watch } = useForm();
+  console.log(watch());
+
+  return (
+    <>
+      <form>
+        <input {...register("email")} placeholder="Email" />
+        <input {...register("firstName")} placeholder="First Name" />
+        <input {...register("lastName")} placeholder="Last Name" />
+        <input {...register("username")} placeholder="Username" />
+        <input {...register("password")} placeholder="Password" />
+        <input {...register("password1")} placeholder="Confirm PassWord" />
+        <button>Add</button>
+      </form>
+    </>
+  );
 }
 
 export default ToDoList;
