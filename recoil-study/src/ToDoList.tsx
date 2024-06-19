5f1b1298c99250b32dc9d2940ab8617d83059ca4
mhadev0703 <minchulha0703@gmail.com>
2024-06-19 19:17:27 -0400

    Modify form validation with formState

--- recoil-study/src/ToDoList.tsx
+++ recoil-study/src/ToDoList.tsx
@@ -31,18 +31,49 @@ import { useForm } from "react-hook-form";
 } */
 
 function ToDoList() {
-  const { register, watch } = useForm();
-  console.log(watch());
+  const { register, handleSubmit, formState } = useForm();
+  const onValid = (data: any) => {
+    console.log(data);
+  };
+  console.log(formState.errors);
 
   return (
     <>
-      <form>
-        <input {...register("email")} placeholder="Email" />
-        <input {...register("firstName")} placeholder="First Name" />
-        <input {...register("lastName")} placeholder="Last Name" />
-        <input {...register("username")} placeholder="Username" />
-        <input {...register("password")} placeholder="Password" />
-        <input {...register("password1")} placeholder="Confirm PassWord" />
+      <form 
+        style={{display: "flex", flexDirection: "column" }} 
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input 
+          {...register("email", { required: true })} 
+          placeholder="Email" 
+        /> 
+        <input {...register("firstName", { required: true })}
+          placeholder="First Name" 
+        />
+        <input {...register("lastName", { required: true })}
+          placeholder="Last Name" 
+        />
+        <input {...register("username", { required: true, minLength: 5 })}
+          placeholder="Username"
+        />
+        <input {...register("password", { 
+            required: "Password is required.",
+            minLength: {
+              value: 10,
+              message: "Your password is too short."
+            },
+          })}
+          placeholder="Password" 
+        />
+        <input {...register("password1", { 
+            required: "Check your password.", 
+            minLength: {
+              value: 10,
+              message: "Your password is too short."
+            }, 
+          })}
+          placeholder="Confirm PassWord" 
+        />
         <button>Add</button>
       </form>
     </>
