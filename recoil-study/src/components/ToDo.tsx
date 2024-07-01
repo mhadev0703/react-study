c83973d972b1a01e7c89bf61c4649a97320fbe1d
mhadev0703 <minchulha0703@gmail.com>
2024-06-30 23:57:07 -0400

    Implement category changing logic

--- recoil-study/src/components/ToDo.tsx
+++ recoil-study/src/components/ToDo.tsx
@@ -1,12 +1,33 @@
-import { IToDo } from "../atoms";
+import { useRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+  const setToDo = useRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event; 
+    console.log("Changed to", event.currentTarget.name);
+  }; 
+
   return (
     <li>
       <span>{text}</span>
-      <button>Doing</button>
-      <button>To Do</button>
-      <button>Done</button>
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )} 
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
     </li>
   );
 }
