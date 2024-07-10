05e86a37f8041c1d443fa0f40e85b22b22a34daf
mhadev0703 <minchulha0703@gmail.com>
2024-07-10 03:58:30 -0400

    Modify categories with enum

--- recoil-study/src/atoms.tsx
+++ recoil-study/src/atoms.tsx
@@ -1,10 +1,21 @@
 import { atom, selector } from "recoil";
 
+export enum Categories {
+  "TO_DO" = "TO_DO",
+  "DOING" = "DOING",
+  "DONE" = "DONE",
+};
+
 export interface IToDo {
   text: string;
   id: number;
-  category: "TO_DO" | "DOING" | "DONE";
+  category: Categories;
 };
+
+export const categoryState = atom<Categories>({
+  key: "category",
+  default: Categories.TO_DO,
+});
 
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
@@ -14,11 +25,8 @@ export const toDoState = atom<IToDo[]>({
 export const toDoSelector = selector({
   key: "toDoSelector",
   get: ({ get }) => {
-    const toDos = get(toDoState)
-    return [
-      toDos.filter((toDo) => toDo.category === "TO_DO"),
-      toDos.filter((toDo) => toDo.category === "DOING"), 
-      toDos.filter((toDo) => toDo.category === "DONE"),  
-    ];
+    const toDos = get(toDoState);
+    const category = get(categoryState);
+    return toDos.filter((toDo) => toDo.category === category);
   },
 });
