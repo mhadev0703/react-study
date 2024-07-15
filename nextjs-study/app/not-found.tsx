f46ed84d3afba1e86f294a834f7fa561bb58aef9
mhadev0703 <minchulha0703@gmail.com>
2024-07-14 22:18:23 -0400

    Add metadata

--- nextjs-study/app/not-found.tsx
+++ nextjs-study/app/not-found.tsx
@@ -1,9 +1,12 @@
-import Navigation from "../components/navigation";
+import { Metadata } from "next";
+
+export const metadata : Metadata = {
+  title: "Not found",
+};
 
 export default function NotFound() {
   return (
     <>
-      <Navigation />
       <h1>Not found!</h1>
     </>
   );
