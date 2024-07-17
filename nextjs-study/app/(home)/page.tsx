4d94e22acfafb91653918e16a0473234df0fe999
mhadev0703 <minchulha0703@gmail.com>
2024-07-16 23:58:30 -0400

    Implement data fetching

--- nextjs-study/app/(home)/page.tsx
+++ nextjs-study/app/(home)/page.tsx
@@ -1,11 +1,24 @@
-export const metadata = {
-  title: "Home",
-};
+"use client"
+
+import { useEffect, useState } from "react";
 
 export default function Page() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies");
+    const json = await response.json();
+    setMovies(json);
+    setIsLoading(false);
+  } 
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
     <>
-      <h1>Hello!</h1>
+      {isLoading ? "Loading..." : JSON.stringify(movies)}
     </>
   );
 }
