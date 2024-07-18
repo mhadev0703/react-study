0858189d029491c3fc2742b35d2f5ed59f9354da
mhadev0703 <minchulha0703@gmail.com>
2024-07-17 23:58:32 -0400

    Implement server side data fetching

--- nextjs-study/app/(home)/page.tsx
+++ nextjs-study/app/(home)/page.tsx
@@ -1,24 +1,20 @@
-"use client"
+export const metadata = {
+  title: "Home",
+};
 
-import { useEffect, useState } from "react";
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
-export default function Page() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies");
-    const json = await response.json();
-    setMovies(json);
-    setIsLoading(false);
-  } 
+async function getMovies() {
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+}
 
-  useEffect(() => {
-    getMovies();
-  }, []);
-
+export default async function HomePage() {
+  const movies = await getMovies();
   return (
     <>
-      {isLoading ? "Loading..." : JSON.stringify(movies)}
+      {JSON.stringify(movies)}
     </>
   );
 }
