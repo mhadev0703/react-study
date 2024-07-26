7050919354736918a8939d8cde9faae445a39da4
mhadev0703 <minchulha0703@gmail.com>
2024-07-26 18:21:10 -0400

    Separate API url as config.ts and update other components using API_URL

--- nextjs-study/components/movie-videos.tsx
+++ nextjs-study/components/movie-videos.tsx
@@ -1,4 +1,4 @@
-import { API_URL } from "../app/(home)/page";
+import { API_URL } from "../app/config";
 import styles from "../styles/movie-videos.module.css";
 
 async function getVideos(id: string) {  
